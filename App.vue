9cdbd1beaed8f67bb9c7e500f93a5ca8e72d1718
Rachid Lamouri <rachidlamouri@gmail.com>
2020-12-12 17:41:59 -0500

    Add initial app state

--- App.vue
+++ App.vue
@@ -1,10 +1,44 @@
 <template>
   <div>
-    Hello
+    <div
+      v-for="{ index, word } in cards"
+      :key="index"
+    >
+      {{ word }}
+    </div>
+    <button @click="reset">
+      Reset
+    </button>
   </div>
 </template>
 
 <script>
+const makeCardState = (word, index) => ({
+  index,
+  word,
+  isAssassin: [false, false],
+  isAgent: [false, false],
+  isBystander: [false, false],
+});
+
+const makePlayerState = () => ({
+  successfulTurnCount: 0,
+});
+
 export default {
+  data() {
+    return {
+      cards: Array.from({ length: 25 }).map((value, index) => makeCardState(index, index)),
+      players: [makePlayerState(), makePlayerState()],
+      sideboard: {
+        maxTurns: 9,
+      },
+    };
+  },
+  methods: {
+    reset() {
+      window.location.reload();
+    },
+  },
 };
 </script>
