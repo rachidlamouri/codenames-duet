16bd0f2a1d624d0b7a71584a02b813e28530145c
Rachid Lamouri <rachidlamouri@gmail.com>
2020-12-25 02:26:15 -0500

    Split store into files for easier testing

---
+++ src/store/mutations.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-param-reassign */
+
+import Vue from 'vue';
+
+export const mutations = {
+  updateSuccessfulTurnCount(state, { playerId, count }) {
+    state.players[playerId].successfulTurnCount = count;
+  },
+  toggleCardState(state, { cardId, key, playerId }) {
+    Vue.set(state.cards[cardId][key], playerId, !state.cards[cardId][key][playerId]);
+  },
+};
